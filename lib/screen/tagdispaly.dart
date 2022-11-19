a9a075b61eb99b66f98a92875d1eedfe5d81a783
Willis-Ponthakorn <willis1234567@gmail.com>
2022-11-19 23:10:39 +0700

    update navigation

---
+++ lib/screen/tagdispaly.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class TagDisplayScreen extends StatefulWidget {
+  const TagDisplayScreen({Key? key}) : super(key: key);
+
+  @override
+  State<TagDisplayScreen> createState() => _TagDisplayScreenState();
+}
+
+class _TagDisplayScreenState extends State<TagDisplayScreen> {
+  @override
+  Widget build(BuildContext context) {}
+}
